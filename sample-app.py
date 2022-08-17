0e032f949872d48b1ba0a4ee8efbbf38acb7a991
nshuti01 <55930674+nshuti01@users.noreply.github.com>
2022-08-17 13:22:33 +0200

    Update sample-app.py

--- sample-app.py
+++ sample-app.py
@@ -5,12 +5,7 @@ import datetime
 # create the application object
 app = Flask(__name__)
 
-# use decorators to link the function to a url
 @app.route('/')
-def home():
-    return "Hello, World!"  # return a string
-
-@app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
 
